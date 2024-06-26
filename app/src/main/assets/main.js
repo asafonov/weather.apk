958ae38d6b3125ef18f87d504319e7a868dad587
asafonov <me@asafonov.org>
2024-06-26 14:02:09 +0000

    perfomance fix

--- app/src/main/assets/main.js
+++ app/src/main/assets/main.js
@@ -389,7 +389,7 @@ window.asafonov.events = {
   CITY_SELECTED: 'CITY_SELECTED'
 }
 window.asafonov.settings = {
-  apiUrl: 'http://isengard.asafonov.org/weather/',
+  apiUrl: 'http://isengard.asafonov.org/api/v1/weather/',
   defaultCity: 'Belgrade'
 }
 window.onerror = (msg, url, line) => {
